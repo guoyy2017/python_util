7779169cc76f6348973044b31d977a80ebc0ea1a
maidou <maidou@bobodeMacBook-Pro-2.local>
2018-04-24 10:43:16 +0800

    retry

---
+++ py_jindutiao/retry_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+@author:maidou
+@contact:QQ4113291000
+@time:18/4/24.上午10:21
+'''
+
+'''
+错误重试模块
+from retry import retry
+
+@retry(tries=5, delay=2)
+def do_something():
+    xxx
+
+do_something()
+'''
+# from retry import retry
+
+if __name__ == '__main__':
+    pass
