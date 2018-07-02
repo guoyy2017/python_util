89886203c52ae9335271e9a0b0620c5f1450b3c0
zenglu <zenglu@trioly.com>
2018-07-02 14:01:49 +0800

    all

---
+++ webui_test/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+@author:maidou
+@contact:QQ4113291000
+@time:2018/5/14.下午4:30
+'''
+
+from flask import Flask
+from WebUI import WebUI
+
+
+
+if __name__ == '__main__':
+    pass
