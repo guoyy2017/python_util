1d6708e73da11a382c00c7521aec3df367f40af5
zenglu <zenglu@trioly.com>
2018-08-02 12:03:13 +0800

    all

--- db/leveldb_test.py
+++ db/leveldb_test.py
@@ -5,10 +5,32 @@
 @author:maidou
 @contact:QQ4113291000
 @time:2018/5/14.下午4:30
+pip install py-leveldb
+def initialize():
+    db = leveldb.LevelDB("students");
+    return db;
+
+def insert(db, sid, name):
+    db.Put(str(sid), name);
+
+def delete(db, sid):
+    db.Delete(str(sid));
+
+def update(db, sid, name):
+    db.Put(str(sid), name);
+
+def search(db, sid):
+    name = db.Get(str(sid));
+    return name;
+
+def display(db):
+    for key, value in db.RangeIter():
+        print (key, value);
+2.7字符串操作需要u''
 '''
 
 from leveldb import LevelDB
 
 db = LevelDB('./data')
-db.Put('nohao','wobuxing')
-print db.Get('nihao')
+db.Put(str(u'nihao'),'wobuxing')
+print db.Get(str(u'nihao'))
--- test.py
+++ test.py
@@ -10,18 +10,45 @@
 菜单命令模式
 '''
 
-from tty_menu import tty_menu
+# from tty_menu import tty_menu
+#
+# l = ['a', 'b', 'c']
+# pos = tty_menu(l, "What is your word?")
+#
+# print("Your word is %s" % (l[pos]))
+#
+# for a in l:
+#     print a
+#
+# for b in range(1,10):
+#     print a
 
-l = ['a', 'b', 'c']
-pos = tty_menu(l, "What is your word?")
+# from ecc import eccrypt
+#
+# eccrypt.encrypt()
+# eccrypt.decrypt()
 
-print("Your word is %s" % (l[pos]))
+import requests
+import hashlib
 
-for a in l:
-    print a
-
-for b in range(1,10):
-    print a
+url = 'http://send.supermms.cn/mt.php'
+appkey = '02940e7646'
+appid = '10568'
+mobile = '13451909511'
+modeid = '211022'
+md5 = hashlib.md5()
+md5.update(appkey+appid+mobile)
+sign = md5.hexdigest()
+data = {
+    "appId": appid,
+    "modeId": modeid,
+    "mobile": mobile,
+    "sign": sign,
+}
+print data
+res = requests.post(url, data=data)
+print res.status_code
+print res.text
 
 
 
